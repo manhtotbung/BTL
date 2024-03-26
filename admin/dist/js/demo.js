b96be7809f49e4b2a087496d69615d1ef5c767bd
manhtotbung <140993971+manhtotbung@users.noreply.github.com>
2024-03-26 22:18:13 +0700

    Update demo.js

--- admin/dist/js/demo.js
+++ admin/dist/js/demo.js
@@ -14,7 +14,8 @@
     if (window.___browserSync___ === undefined && Number(localStorage.getItem('AdminLTE:Demo:MessageShowed')) < Date.now()) {
       localStorage.setItem('AdminLTE:Demo:MessageShowed', (Date.now()) + (15 * 60 * 1000))
       // eslint-disable-next-line no-alert
-      alert('You load AdminLTE\'s "demo.js", \nthis file is only created for testing purposes!')
+      alert('Chào mừng đến với Book Store!')
+      
     }
   }, 1000)
 
